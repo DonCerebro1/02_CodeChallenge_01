af37b6bb580140207fe4343522b1f777ed2404ad
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-11-18 14:19:09 +0100

    challenge_loesung

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,8 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+const numOne = parseInt(prompt ("Enter first number: "));
+const numTwo = parseInt(prompt ("Enter second number: "));5
+let sum = numOne + numTwo;
+console.log("The sum of both numbers is: " + sum);
